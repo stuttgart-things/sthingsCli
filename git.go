da91619c70dbf09684e0ef8ced2b496227901f81
Patrick Hermann <patrick.hermann@sva.de>
2023-08-09 19:10:27 +0000

    updated sthingsCli 23.0809.1910 for tag version v0.1.19

--- git.go
+++ git.go
@@ -6,6 +6,7 @@ package cli
 
 import (
 	"fmt"
+	"os"
 
 	billy "github.com/go-git/go-billy/v5"
 	plumbing "github.com/go-git/go-git/v5/plumbing"
@@ -17,17 +18,18 @@ import (
 	git "github.com/go-git/go-git/v5"
 )
 
-func CloneGitRepository(repository, branchName string, auth *http.BasicAuth) (fs billy.Filesystem, cloned bool) {
+func CloneGitRepository(repository, branchName, commitId string, auth *http.BasicAuth) (fs billy.Filesystem, cloned bool) {
 
 	// Init memory storage and fs
 	storer := memory.NewStorage()
 	fs = memfs.New()
 
 	// Clone repo into memfs
-	_, err := git.Clone(storer, fs, &git.CloneOptions{
+	r, err := git.Clone(storer, fs, &git.CloneOptions{
 		URL:           repository,
 		Auth:          auth,
 		RemoteName:    "origin",
+		Progress:      os.Stdout,
 		ReferenceName: plumbing.ReferenceName(branchName),
 	})
 
@@ -36,6 +38,27 @@ func CloneGitRepository(repository, branchName string, auth *http.BasicAuth) (fs
 	} else {
 		fmt.Println("Repository cloned")
 		cloned = true
+	}
+
+	// CHECK OUT SPECIFIC COMMIT BY ID (IF COMMIT IS GIVEN)
+
+	if commitId != "" {
+
+		ref, err := r.Head()
+		fmt.Println(ref, err)
+
+		w, err := r.Worktree()
+		fmt.Println(err)
+
+		err = w.Checkout(&git.CheckoutOptions{
+			Hash: plumbing.NewHash(commitId),
+		})
+
+		fmt.Println(err)
+
+		ref, _ = r.Head()
+
+		fmt.Println(ref.Hash())
 	}
 
 	return
--- git_test.go
+++ git_test.go
@@ -5,21 +5,26 @@ Copyright © 2023 Patrick Hermann patrick.hermann@sva.de
 package cli
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 var (
-	repo       = "https://github.com/stuttgart-things/machineShop.git"
-	branchName = "main"
+	repo                  = "https://github.com/stuttgart-things/machineShop.git"
+	branchName            = "main"
+	commitID              = "1ce75f510bff3b95b0a5998ee22731ec058c3267"
+	expectedFileList      = []string{".gitignore", "LICENSE", "README.md"}
+	expectedDirectoryList = []string{}
 )
 
 func TestCloneGitRepository(t *testing.T) {
 
 	assert := assert.New(t)
 
-	_, cloned := CloneGitRepository(repo, branchName, nil)
+	_, cloned := CloneGitRepository(repo, branchName, "", nil)
 
 	assert.Equal(cloned, true)
 
@@ -27,9 +32,20 @@ func TestCloneGitRepository(t *testing.T) {
 
 func TestGetFileListFromGitRepository(t *testing.T) {
 
-	assert := assert.New(t)
+	var fileList []string
+	var directoryList []string
 
-	_, cloned := CloneGitRepository(repo, branchName, nil)
-	assert.Equal(cloned, true)
+	repo, cloned := CloneGitRepository(repo, branchName, commitID, nil)
+
+	if cloned {
+		fileList, directoryList = GetFileListFromGitRepository("", repo)
+		fmt.Println(fileList, directoryList)
+	}
+
+	if !reflect.DeepEqual(fileList, expectedFileList) && reflect.DeepEqual(directoryList, expectedDirectoryList) {
+		t.Errorf("expected lists differ")
+	} else {
+		fmt.Println("test successfully")
+	}
 
 }
