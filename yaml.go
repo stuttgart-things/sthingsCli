8988e3bc63f7c92e70bbe518d11d480ddad68ec0
Patrick Hermann <patrick.hermann@sva.de>
2023-08-14 09:46:12 +0000

    updated sthingsCli 23.0814.0946 for tag version v0.1.22

--- yaml.go
+++ yaml.go
@@ -5,6 +5,7 @@ Copyright © 2023 Patrick Hermann patrick.hermann@sva.de
 package cli
 
 import (
+	"bytes"
 	"fmt"
 	"path/filepath"
 	"strings"
@@ -19,6 +20,26 @@ func ReadYamlToObject(pathToYAML, fileExtension string, yamlObject interface{}) 
 	viper.AddConfigPath(filepath.Dir(pathToYAML))
 
 	if err := viper.ReadInConfig(); err != nil {
+		return yamlObject
+	}
+
+	if err := viper.Unmarshal(&yamlObject); err != nil {
+		fmt.Println(err)
+		return yamlObject
+	}
+
+	return yamlObject
+
+}
+
+func ReadInlineYamlToObject(inlineYAML []byte, yamlObject interface{}) interface{} {
+
+	viper.SetConfigType("yaml")
+
+	// READ YAML
+	viper.ReadConfig(bytes.NewBuffer(inlineYAML))
+
+	if err := viper.ReadConfig(bytes.NewBuffer(inlineYAML)); err != nil {
 		return yamlObject
 	}
 
--- yaml_test.go
+++ yaml_test.go
@@ -5,9 +5,41 @@ Copyright © 2023 Patrick Hermann patrick.hermann@sva.de
 package cli
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestReadYamlToObject(t *testing.T) {
+var config Profile
 
+type Install struct {
+	Url string `mapstructure:"url"`
+	Bin string `mapstructure:"bin"`
+}
+
+type Script struct {
+	Script string `mapstructure:"script"`
+}
+
+type Profile struct {
+	BinaryProfile []map[string]Install `mapstructure:"binary"`
+	ScriptProfile []map[string]Script  `mapstructure:"script"`
+}
+
+var yamlExample = []byte(`
+binary:
+  - argocd:
+      url: https://github.com/argoproj/argo-cd/releases/download/v{{ or .decksmanVersion "2.5.10" }}/argocd-linux-amd64
+      bin: argocd-linux-amd64
+  - decksman:
+      url: https://artifacts.tiab.labda.sva.de/decksman/decksman-{{ or .decksmanVersion "0.8.436" }}.zip
+      bin: decksman
+script:
+  - argocd:
+      script: |
+        echo hello
+`)
+
+func TestRReadInlineYamlToObject(t *testing.T) {
+	config := ReadInlineYamlToObject(yamlExample, config).(Profile)
+	fmt.Println(config)
 }
