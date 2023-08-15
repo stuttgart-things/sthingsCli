45b22e98363ffae4cbeeb865776d832d340acc94
Patrick Hermann <patrick.hermann@sva.de>
2023-08-15 11:49:35 +0000

    updated sthingsCli 23.0815.1149 for tag version v0.1.25

--- webrequests_test.go
+++ webrequests_test.go
@@ -3,3 +3,14 @@ Copyright © 2023 Patrick Hermann patrick.hermann@sva.de
 */
 
 package cli
+
+import "testing"
+
+var (
+	downloadURL    = "https://dl.k8s.io/release/v1.27.4/bin/linux/amd64/kubectl"
+	downloadTarget = "/tmp"
+)
+
+func TestDownloadFileWithProgressBar(t *testing.T) {
+	DownloadFileWithProgressBar(downloadURL, downloadTarget)
+}
