3aeddde2f979f5f8f9babf5c0c754bd11229c0eb
Patrick Hermann <patrick.hermann@sva.de>
2023-08-19 15:06:36 +0000

    updated sthingsCli 23.0819.1506 for tag version v0.1.27

--- redis_test.go
+++ redis_test.go
@@ -3,3 +3,43 @@ Copyright © 2023 Patrick Hermann patrick.hermann@sva.de
 */
 
 package cli
+
+// var ctx = context.Background()
+
+// // Name - student name
+// type Name struct {
+// 	First  string `json:"first,omitempty"`
+// 	Middle string `json:"middle,omitempty"`
+// 	Last   string `json:"last,omitempty"`
+// }
+
+// // Student - student object
+// type Student struct {
+// 	Name Name `json:"name,omitempty"`
+// 	Rank int  `json:"rank,omitempty"`
+// }
+
+// func TestSetObjectToRedisJSON(t *testing.T) {
+
+// 	// INITALIZE REDIS
+// 	var addr = flag.String("Server", "localhost:6379", "Redis server address")
+
+// 	redisJSONHandler := rejson.NewReJSONHandler()
+// 	flag.Parse()
+
+// 	redisClient := goredis.NewClient(&goredis.Options{Addr: *addr, DB: 0})
+
+// 	redisJSONHandler.SetGoRedisClient(redisClient)
+
+// 	student := Student{
+// 		Name: Name{
+// 			"Patrick",
+// 			"Johannes",
+// 			"Hermann",
+// 		},
+// 		Rank: 1,
+// 	}
+
+// 	SetObjectToRedisJSON(redisJSONHandler, student, "student456")
+
+// }
