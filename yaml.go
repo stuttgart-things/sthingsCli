de6db136f6a52f6895da2464180d8ca8497f2a17
Patrick Hermann <patrick.hermann@sva.de>
2023-09-14 14:50:13 +0000

    updated sthingsCli 23.0914.1449 for tag version v0.1.37

--- yaml.go
+++ yaml.go
@@ -7,6 +7,7 @@ package cli
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"path/filepath"
 	"strings"
 
@@ -14,6 +15,23 @@ import (
 
 	"github.com/spf13/viper"
 )
+
+func ReadYamlKeyValuesFromFile(yamlFileContent []byte) (yamlStructure map[string]interface{}) {
+
+	yamlStructure = make(map[string]interface{})
+	data := make(map[interface{}]interface{})
+
+	err := yaml.Unmarshal(yamlFileContent, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for k, v := range data {
+		yamlStructure[fmt.Sprintf("%v", k)] = fmt.Sprintf("%v", v)
+	}
+
+	return
+}
 
 func ReadYamlToObject(pathToYAML, fileExtension string, yamlObject interface{}) interface{} {
 
