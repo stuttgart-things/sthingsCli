3f58091f2d33fdcfe1748df068f87dccd618b755
Patrick Hermann <patrick.hermann@sva.de>
2024-09-15 12:22:14 +0000

    updated sthingsCli 24.0915.1221 for tag version v0.1.123

--- verify_test.go
+++ verify_test.go
@@ -2,50 +2,46 @@ package cli
 
 import (
 	"testing"
-	// sthingsBase "github.com/stuttgart-things/sthingsBase"
+
+	sthingsBase "github.com/stuttgart-things/sthingsBase"
 )
 
 func TestVerifyReadKeyValues(t *testing.T) {
-	// Beispiel-Logger
-	// logger := sthingsBase.StdOutFileLogger("/tmp/machineShop.log", "2006-01-02 15:04:05", 50, 3, 28)
 
-	// tests := []struct {
-	// 	templateValues []string
-	// 	enableVault    bool
-	// 	expectedResult map[string]interface{}
-	// }{
-	// 	{
-	// 		templateValues: []string{"username=admin", "password=secret"},
-	// 		enableVault:    false,
-	// 		expectedResult: map[string]interface{}{
-	// 			"username=admin":  "username=admin",
-	// 			"password=secret": "password=secret",
-	// 		},
-	// 	},
-	// 	{
-	// 		templateValues: []string{"key1=value1", "key2=value2"},
-	// 		enableVault:    true,
-	// 		expectedResult: map[string]interface{}{
-	// 			"key1=value1": "key1=value1",
-	// 			"key2=value2": "key2=value2",
-	// 		},
-	// 	},
-	// 	{
-	// 		templateValues: []string{},
-	// 		enableVault:    false,
-	// 		expectedResult: map[string]interface{}{},
-	// 	},
-	// }
+	logger := sthingsBase.StdOutFileLogger("/tmp/machineShop.log", "2006-01-02 15:04:05", 50, 3, 28)
 
-	// for _, test := range tests {
-	// 	result := VerifyReadKeyValues(test.templateValues, logger, test.enableVault)
-	// 	if len(result) != len(test.expectedResult) {
-	// 		t.Errorf("Expected result length: %d, got: %d", len(test.expectedResult), len(result))
-	// 	}
-	// 	for key, expectedValue := range test.expectedResult {
-	// 		if value, exists := result[key]; !exists || value != expectedValue {
-	// 			t.Errorf("For key '%s', expected value: '%v', got: '%v'", key, expectedValue, value)
-	// 		}
-	// 	}
-	// }
+	tests := []struct {
+		templateValues []string
+		enableVault    bool
+		expectedResult map[string]interface{}
+	}{
+		{
+			templateValues: []string{"username=admin", "password=secret"},
+			enableVault:    false,
+			expectedResult: map[string]interface{}{
+				"username": "admin",
+				"password": "secret",
+			},
+		},
+		// {
+		// 	templateValues: []string{"key1=value1", "key2=value2"},
+		// 	enableVault:    true,
+		// 	expectedResult: map[string]interface{}{
+		// 		"key1=value1": "key1=value1",
+		// 		"key2=value2": "key2=value2",
+		// 	},
+		// },
+	}
+
+	for _, test := range tests {
+		result := VerifyReadKeyValues(test.templateValues, logger, test.enableVault)
+		if len(result) != len(test.expectedResult) {
+			t.Errorf("Expected result length: %d, got: %d", len(test.expectedResult), len(result))
+		}
+		for key, expectedValue := range test.expectedResult {
+			if value, exists := result[key]; !exists || value != expectedValue {
+				t.Errorf("For key '%s', expected value: '%v', got: '%v'", key, expectedValue, value)
+			}
+		}
+	}
 }
